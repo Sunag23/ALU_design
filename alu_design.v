0cc13c5aa2b9dde41751612be9491b111e87539b
T G Sunag <152414924+Sunag23@users.noreply.github.com>
2026-05-06 21:47:20 +0530

    Refactor ALU design with improved count handling

--- alu_design.v
+++ alu_design.v
@@ -17,306 +17,329 @@ output reg cout,
 output reg G,
 output reg L,
 output reg E
- );
+);
 
- reg [1:0] count;
- always @(posedge clk or posedge rst)
- begin
- if(rst)
-    count<=2'b00;
- else if(cmd==4'd9)
-    count<=count+1;
- else
-     count<=0;
- end
- 
- always @(posedge clk or posedge rst)
- begin
- if(rst)
- begin
- res<={2*N{1'bz}};
- err<=1'b0;
- OFLOW<=1'b0;
- G<=1'b0;
- L<=1'b0;
- E<=1'b0;
- cout<=1'b0;
- end
- else if(!CE)
- begin
- res<=res;
- err<=err;
- OFLOW<=OFLOW;
- G<=G;
- L<=L;
- E<=E;
- cout<=cout;
- end
- else if (mode)
- begin
- case(cmd)
- 
- 4'd0:begin
- if(input_valid==2'b11)
- begin
- res<=OPA+OPB;
- cout<=res[9];
- OFLOW<=1'b0;
- {err,G,E,L}<=4'b0000;
- end
- else
- begin
- err<=1'b1;
- {res,OFLOW,cout,G,L,E}<={res,OFLOW,cout,G,L,E};
- end
- end
- 
- 4'd1:begin
- if(input_valid==2'b11)
- begin
- res<=OPA-OPB;
- cout<=(OPA>=OPB);
- OFLOW<=!(OPA>=OPB);
- {err,G,E,L}<=4'b0000;
- end
- else
- begin
- err<=1'b1;
- {res,OFLOW,cout,G,L,E}<={res,OFLOW,cout,G,L,E};
- end
- end
- 
- 4'd2:begin
-     if(input_valid==2'b11)
-      begin
-        res<=OPA+OPB+cin;
-        cout<=(OPA+OPB+cin)>>N;
-        OFLOW<=1'b0;
-        {err,G,E,L}<=4'b0000;
-     end
- else
+reg [1:0] count;
+
+always @(posedge clk or posedge rst)
+begin
+    if(rst)
+        count <= 0;
+
+    else if(CE)
     begin
-        err<=1'b1;
-       {res,OFLOW,cout,G,L,E}<={res,OFLOW,cout,G,L,E};
+      if(mode && (cmd==4'd9 || cmd==4'd10))
+        begin
+            if(count < 2)
+                count <= count + 1;
+            else
+                count <= 0;
+        end
+        else
+        begin
+            if(count < 1)
+                count <= count + 1;
+            else
+                count <= 0;
+        end
     end
- end 
- 
- 4'd3:begin
-    if(input_valid==2'b11)
-     begin
-        res<=OPA-OPB-cin;
-        cout<=({1'b0,OPA}>={1'b0,OPB}+cin);
-        OFLOW<=({1'b0, OPA} < ({1'b0, OPB} + cin));
-        {err,G,E,L}<=4'b0000;
-    end
- else
-    begin
-        err<=1'b1;
-        {res,OFLOW,cout,G,L,E}<={res,OFLOW,cout,G,L,E};
-    end
- end 
- 
+end
+
+always @(posedge clk or posedge rst)
+begin
+
+if(rst)
+begin
+res<={2*N{1'bz}};
+err<=1'b0;
+OFLOW<=1'b0;
+G<=1'b0;
+L<=1'b0;
+E<=1'b0;
+cout<=1'b0;
+end
+
+else if(!CE)
+begin
+res<=res;
+err<=err;
+OFLOW<=OFLOW;
+G<=G;
+L<=L;
+E<=E;
+cout<=cout;
+end
+
+else if(mode &&
+(
+ ((cmd==4'd9 || cmd==4'd10 || cmd==4'd11 || cmd==4'd12) && count==2'd2)
+ ||
+ (!(cmd==4'd9 || cmd==4'd10 || cmd==4'd11 || cmd==4'd12) && count==2'd1)
+))
+begin
+
+case(cmd)
+
+4'd0:begin
+if(input_valid==2'b11)
+begin
+res<=OPA+OPB;
+cout<=({1'b0, OPA} + {1'b0, OPB}) >> N;
+{OFLOW,err,G,E,L}<=5'b00000;
+end
+else
+begin
+err<=1'b1;
+{res,OFLOW,cout,G,L,E}<={res,OFLOW,cout,G,L,E};
+end
+end
+
+4'd1:begin
+if(input_valid==2'b11)
+begin
+res<=OPA-OPB;
+cout<=(OPA>=OPB);
+OFLOW<=!(OPA>=OPB);
+{err,G,E,L}<=4'b0000;
+end
+else
+begin
+err<=1'b1;
+{res,OFLOW,cout,G,L,E}<={res,OFLOW,cout,G,L,E};
+end
+end
+
+4'd2:begin
+if(input_valid==2'b11)
+begin
+res<=OPA+OPB+cin;
+cout<=(OPA+OPB+cin)>>N;
+OFLOW<=1'b0;
+{err,G,E,L}<=4'b0000;
+end
+else
+begin
+err<=1'b1;
+{res,OFLOW,cout,G,L,E}<={res,OFLOW,cout,G,L,E};
+end
+end
+
+4'd3:begin
+if(input_valid==2'b11)
+begin
+res<=OPA-OPB-cin;
+cout<=({1'b0,OPA}>={1'b0,OPB}+cin);
+OFLOW<=({1'b0, OPA} < ({1'b0, OPB} + cin));
+{err,G,E,L}<=4'b0000;
+end
+else
+begin
+err<=1'b1;
+{res,OFLOW,cout,G,L,E}<={res,OFLOW,cout,G,L,E};
+end
+end
+
 4'd4:
 begin
 if(input_valid[0])
-    begin
-        res<=OPA+1;
-        cout<=(OPA==0);
-        OFLOW<=1'b0;
-        {err,G,E,L}<=4'b0000;
-    end
- else
-    begin
-        err<=1'b1;
-        {res,OFLOW,cout,G,L,E}<={res,OFLOW,cout,G,L,E};
-    end
- end
- 
- 
- 4'd5:
+begin
+res<=OPA+1;
+cout<=(OPA==0);
+OFLOW<=1'b0;
+{err,G,E,L}<=4'b0000;
+end
+else
+begin
+err<=1'b1;
+{res,OFLOW,cout,G,L,E}<={res,OFLOW,cout,G,L,E};
+end
+end
+
+4'd5:
 begin
 if(input_valid[0])
-    begin
-        res<=OPA-1;
-        cout<=(OPA==0);
-        OFLOW<=1'b0;
-        {err,G,E,L}<=4'b0000;
-    end
- else
-    begin
-        err<=1'b1;
-        {res,OFLOW,cout,G,L,E}<={res,OFLOW,cout,G,L,E};
-    end
- end
- 
-  4'd6:
+begin
+res<=OPA-1;
+cout<=(OPA==0);
+OFLOW<=1'b0;
+{err,G,E,L}<=4'b0000;
+end
+else
+begin
+err<=1'b1;
+{res,OFLOW,cout,G,L,E}<={res,OFLOW,cout,G,L,E};
+end
+end
+
+4'd6:
 begin
 if(input_valid[1])
-    begin
-        res<=OPB+1;
-        cout<=(OPB==0);
-        OFLOW<=1'b0;
-        {err,G,E,L}<=4'b0000;
-    end
- else
-    begin
-        err<=1'b1;
-        {res,OFLOW,cout,G,L,E}<={res,OFLOW,cout,G,L,E};
-    end
- end
- 
- 
- 4'd7:
+begin
+res<=OPB+1;
+cout<=(OPB==0);
+OFLOW<=1'b0;
+{err,G,E,L}<=4'b0000;
+end
+else
+begin
+err<=1'b1;
+{res,OFLOW,cout,G,L,E}<={res,OFLOW,cout,G,L,E};
+end
+end
+
+4'd7:
 begin
 if(input_valid[1])
-    begin
-        res<=OPB-1;
-        cout<=(OPB==0);
-        OFLOW<=1'b0;
-        
-    end
- else
-    begin
-        err<=1'b1;
-        {res,OFLOW,cout,G,L,E}<={res,OFLOW,cout,G,L,E};
-    end
- end
- 
- 4'd8:
+begin
+res<=OPB-1;
+cout<=(OPB==0);
+OFLOW<=1'b0;
+end
+else
+begin
+err<=1'b1;
+{res,OFLOW,cout,G,L,E}<={res,OFLOW,cout,G,L,E};
+end
+end
+
+4'd8:
 begin
 if(input_valid==2'b11)
-    begin
-        {err,cout,OFLOW}<=4'b0000;  
-        res<={N{2'b00}};
-        {G,E,L}<={(OPA>OPB),(OPA==OPB),(OPA<OPB)};
-         
-    end
- else
-    begin
-        err<=1'b1;
-        {res,OFLOW,cout,G,L,E}<={res,OFLOW,cout,G,L,E};
-    end
- end
- 
- 4'd9:
- begin
- if(input_valid==2'b11)
-    begin
-    if(count==2'b10)
-        res<=(OPA+1)*(OPB+1);
-    else
-        res<=res;
-    end 
- else
- begin
-   err<=1'b1;
-        {res,OFLOW,cout,G,L,E}<={res,OFLOW,cout,G,L,E};
-        end
-    end
-    
- 4'd10:begin
- if(input_valid==2'b11)
-    begin
-    if(count==2'b10)
-    begin
-    res<=(OPA<<1)*OPB;
-    {OFLOW,cout,G,L,E}<=5'b00000;
-    end
-    end
- else
-    begin
-        err<=1'b1;
-        {res,OFLOW,cout,G,L,E}<={res,OFLOW,cout,G,L,E};
-      end
-      end
- 
- 4'd11:begin
+begin
+{err,cout,OFLOW}<=3'b000;
+res<={2*N{1'b0}};
+{G,E,L}<={(OPA>OPB),(OPA==OPB),(OPA<OPB)};
+end
+else
+begin
+err<=1'b1;
+{res,OFLOW,cout,G,L,E}<={res,OFLOW,cout,G,L,E};
+end
+end
+
+4'd9:
+begin
+if(input_valid==2'b11)
+begin
+res<=(OPA+1)*(OPB+1);
+end
+else
+begin
+err<=1'b1;
+{res,OFLOW,cout,G,L,E}<={res,OFLOW,cout,G,L,E};
+end
+end
+
+4'd10:begin
+if(input_valid==2'b11)
+begin
+res<=(OPA<<1)*OPB;
+{OFLOW,cout,G,L,E}<=5'b00000;
+end
+else
+begin
+err<=1'b1;
+{res,OFLOW,cout,G,L,E}<={res,OFLOW,cout,G,L,E};
+end
+end
+
+4'd11:begin
+if(input_valid==2'b11)
+begin
+res<=$signed(OPA)+$signed(OPB);
+OFLOW<= ((OPA[N-1] == OPB[N-1]) && (res[N-1] != OPA[N-1]));
+{G,L,E}<={($signed(OPA)>$signed(OPB)),($signed(OPA)<$signed(OPB)),($signed(OPA)==$signed(OPB))};
+cout<=0;
+end
+else
+err<=1'b1;
+end
+
+4'd12:begin
+if(input_valid==2'b11)
+begin
+res<=$signed(OPA)-$signed(OPB);
+OFLOW<= ((OPA[N-1] != OPB[N-1]) && (res[N-1] != OPA[N-1]));
+{G,L,E}<={($signed(OPA)>$signed(OPB)),($signed(OPA)<$signed(OPB)),($signed(OPA)==$signed(OPB))};
+cout<=0;
+end
+else
+err<=1'b1;
+end
+
+endcase
+end
+
+  else if(!mode)
+begin
+
+{err,OFLOW,cout,G,E,L} <= 6'b0;
+
+case(cmd)
+
+4'd0:begin
   if(input_valid==2'b11)
-  begin
-   if(count==2'b10)
-   begin
-  res<=$signed(OPA)+$signed(OPB);
-  OFLOW<= ((OPA[N-1] == OPB[N-1]) && (res[N-1] != res[N-1]));
-  {G,L,E}<={($signed(OPA)>$signed(OPB)),($signed(OPA)<$signed(OPB)),($signed(OPA)==$signed(OPB))};
-  cout<=0;
-  end
-  else
-    err<=1'b1;
-  end
-  end
-  
- 4'd12:begin
- if(input_valid==2'b11)
- begin
- if(count==2'b10)
-   begin
-  res<=$signed(OPA)-$signed(OPB);
-  OFLOW<= (OPA[N-1] != OPB[N-1]) && (res[N-1] != res[N-1]);
-  {G,L,E}<={($signed(OPA)>$signed(OPB)),($signed(OPA)<$signed(OPB)),($signed(OPA)==$signed(OPB))};
-  cout<=0;
-  end
-  else
-    err<=1'b1;
-  end
-  end
-  endcase
- end
- 
- else
-  begin
-   {err,OFLOW,cout,G,E,L} <= 6'b0;
-    case(cmd)
-    4'd0:begin
-     if(input_valid==2'b11)
-        res<=OPA&OPB;
-     end
-    4'd1:begin
-     if(input_valid==2'b11)
-        res<=~(OPA&OPB);
-     end
-    4'd2:begin
-    if(input_valid==2'b11)
-        res<=OPA|OPB;
-    end
-    4'd3:begin
-    if(input_valid==2'b11)
-        res<=~(OPA|OPB);
-    end 
-    4'd4:begin
-    if(input_valid==2'b11)
-        res<=OPA^OPB;
-    end
-    4'd5:begin
-    if(input_valid==2'b11)
-        res<=~(OPA^OPB);
-    end 
-    4'd6:begin
-    if(input_valid[0])
-        res<=~(OPA);
-    end
-    4'd7:begin
-    if(input_valid[1])
-        res<=~(OPB);
-    end
-    4'd8:begin
-    if(input_valid[0])
-    res<=OPA>>1;
-    end
-    4'd9:begin
-    if(input_valid[0])
-    res<=OPA<<1;
-    end
-    4'd10:begin
-    if(input_valid[1])
-    res<=OPB>>1;
-    end
-    4'd11:begin
-    if(input_valid[1])
-    res<=OPB<<1;
-    end
-    4'd12:begin
-    if(input_valid==2'b11)
-    begin
+res<=OPA&OPB;
+end
+
+4'd1:begin
+  if(input_valid==2'b11)
+res<=~(OPA&OPB);
+end
+
+4'd2:begin
+if(input_valid==2'b11)
+  res<=(OPA|OPB);
+end
+
+4'd3:begin
+if(input_valid==2'b11)
+res<=~(OPA|OPB);
+end
+
+4'd4:begin
+if(input_valid==2'b11)
+res<=OPA^OPB;
+end
+
+4'd5:begin
+if(input_valid==2'b11)
+res<=~(OPA^OPB);
+end
+
+4'd6:begin
+if(input_valid[0])
+res<=~(OPA);
+end
+
+4'd7:begin
+if(input_valid[1])
+res<=~(OPB);
+end
+
+4'd8:begin
+if(input_valid[0])
+res<=OPA>>1;
+end
+
+4'd9:begin
+if(input_valid[0])
+res<=OPA<<1;
+end
+
+4'd10:begin
+if(input_valid[1])
+res<=OPB>>1;
+end
+
+4'd11:begin
+if(input_valid[1])
+res<=OPB<<1;
+end
+
+4'd12:begin
+if(input_valid==2'b11)
+begin
     casex(OPB)
     8'b0000x000:res=OPA;
     8'b0000x001:res={OPA[N-1:1],OPA[0]};
@@ -329,12 +352,13 @@ if(input_valid==2'b11)
     default:err=1'b1;
     endcase
     end
-    else
-    err<=1'b1;
-    end
-    4'd13:begin
-    if(input_valid==2'b11)
-    begin
+else
+err<=1'b1;
+end
+
+4'd13:begin
+if(input_valid==2'b11)
+ begin
     casex(OPB)
     8'b0000x000:res=OPA;
     8'b0000x001:res={OPA[0],OPA[N-1:1]};
@@ -346,12 +370,13 @@ if(input_valid==2'b11)
     8'b0000x111:res={OPA[0],OPA[N-1:1]};
     default:err=1'b1; 
    endcase
-    end
-    else
-    err<=1'b1;
-    end
-    endcase
  end
- end
-endmodule
+err<=1'b1;
+end
 
+endcase
+end
+
+end
+
+endmodule
